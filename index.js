fb6ba49d39936b9404dcb8172a662a5ea9854659
irene29-hub <muemarenee@gmail.com>
2025-06-05 02:20:47 +0300

    adds math

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+// 1. Multiply num1 and num2 so the result is 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
+// 2. Generate a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+// 3. Calculate the remainder of num3 divided by num4 so the remainder is 4
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+
+// 4. Find the highest number in a set so the value is 20
+const max = Math.max(1, 5, 10, 20);
 // Write your code here
